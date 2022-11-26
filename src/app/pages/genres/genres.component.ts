70ab6c860b481e5462cb0584a63f44e52051814f
Allahveren Mikayilov <mikayilov.allahveren@gmail.com>
2022-11-26 15:29:40 +0400

    first commit

---
+++ src/app/pages/genres/genres.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { MoviesService } from "../../services/movies.service";
+import { TvShowsService } from "../../services/tvshows.service";
+import { Genre } from "../../models/genre";
+
+@Component({
+  selector: "app-genres",
+  templateUrl: "./genres.component.html",
+  styleUrls: ["./genres.component.scss"],
+})
+export class GenresComponent implements OnInit {
+  genres: Genre[] = [];
+  tvShowGenres: Genre[] = [];
+
+  constructor(
+    private moviesService: MoviesService,
+    private tvShowsService: TvShowsService
+  ) {}
+
+  ngOnInit(): void {
+    this.moviesService.getMoviesGenres().subscribe((genresData) => {
+      this.genres = genresData;
+    });
+    this.tvShowsService.getTvShowsGenres().subscribe((genresData) => {
+      this.tvShowGenres = genresData;
+    });
+  }
+}
